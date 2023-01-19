29ee8af49747c581ff14a9cd11d983090641f591
SHETH MANAV PRASHANT <shethmanav2002@gmail.com>
2023-01-19 11:18:24 +0000

    Update index.html and EventsName.jsx

--- src/Pages/EventsPage/EventsName.jsx
+++ src/Pages/EventsPage/EventsName.jsx
@@ -4,7 +4,10 @@ function EventsName(props) {
   return (
     <div
       data-aos={props.item.Animation}
-      data-aos-delay="100"
+      data-aos-delay="5000"
+      data-aos-duration="4000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
       className={`EVENT-ITEM-CONTAINER ${props.item.subClassName}`}
     >
       <img src={props.item.EventimageSource} alt="" />
