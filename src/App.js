a7dcc8dd0c4c8d538bb5e86cc96030ed355a4cac
SHETH MANAV PRASHANT <shethmanav2002@gmail.com>
2023-01-21 20:14:22 +0000

    Update 7 files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import EventsPage from "./Pages/EventsPage/EventsPage";
 import AboutPage from "./Pages/AboutPage/AboutPage";
 import SponsersPage from "./Pages/SponsersPage/SponsersPage";
 import ParticipantsPage from "./Pages/ParticipantsPage/ParticipantsPage";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./Pages/Navbar/Navbar";
 import HomePage from "./Pages/HomePage/HomePage";
 import Footer from "./Pages/Footer/Footer";
 import LoginPage from "./Pages/LoginPage/LoginPage";
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import './HomePage.css'
+import React from "react";
+import "./HomePage.css";
+import "../Navbar/Navbar.css";
 function HomePage() {
   return (
-    <div className='HOMEPAGE-MAIN-CONTAINER'>
-           <div className='headerline'>
-             <h1> PRAKARSH'<span>23</span></h1>  
-             <h2> ENGAGING mINds, eMpowERiNg sUccEss
-</h2>
-             <p> Prakarsh 23's vision is to assist the rural areas, and villages and to encourage them in the advancement of their lives through technologies developed in Prakarsh by students. </p>
+    <div className="HOMEPAGE-MAIN-CONTAINER">
+      <div className="headerline">
+        <h1>
+          {" "}
+          PRAKARSH'<span>23</span>
+        </h1>
+        <h2> ENGAGING mINds, eMpowERiNg sUccEss</h2>
+        <p>
+          {" "}
+          Prakarsh 23's vision is to assist the rural areas, and villages and to
+          encourage them in the advancement of their lives through technologies
+          developed in Prakarsh by students.{" "}
+        </p>
 
-            <button type='button' className='expbtn'><a href='#'>Explore Events  </a> </button>
+        {/* <button type="button" className="signupbtn expbtn">
+          <a href="#">Explore Events </a>{" "}
+        </button> */}
 
-             {/* <div className='launch'>
-             <h1> 1-2 MARCH</h1>
-           </div> */}
-          </div>
-        
-       
-    </div>  
-  )
+        <div className="launch">
+          <h1 className="glow"> coming soon...</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
